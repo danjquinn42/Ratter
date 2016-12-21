387ca0ad9b7126d80f36f194d370b5a39998cdeb
danjquinn42 <danjquinn42@gmail.com>
2016-12-21 17:29:47 -0500

    Add skeleton and sprites

---
+++ lib/ratter.jsx
@@ -0,0 +1,14 @@
+// let stage, stageWidth, stageHeight;
+
+document.body.addEventListener('DOMContentLoaded', init());
+
+
+function init() {
+  var stage = new createjs.Stage("demoCanvas");
+  var circle = new createjs.Shape();
+  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+  circle.x = 100;
+  circle.y = 100;
+  stage.addChild(circle);
+  stage.update();
+}
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: './lib/ratter.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
