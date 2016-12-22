cbf0c8c0d81742d4f4aa3ce0ac02ad3f314924a9
danjquinn42 <danjquinn42@gmail.com>
2016-12-22 12:23:29 -0500

    Add sprites and animations

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   context: __dirname,
-  entry: './lib/ratter.jsx',
+  entry: './lib/ratter.js',
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
