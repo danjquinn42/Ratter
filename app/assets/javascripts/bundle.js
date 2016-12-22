1b3e638d2249df3885a5f58aff2bdef0f7eb2d01
danjquinn42 <danjquinn42@gmail.com>
2016-12-22 12:59:56 -0500

    Add sprites and animations

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -45,14 +45,14 @@
 /***/ function(module, exports) {
 
 	document.addEventListener('DOMContentLoaded', init);
-	
+
 	let stage, width, height, loader;
 	let rat;
-	
+
 	const handleComplete = () => {
 	  // rat = new createjs.Shape();
 	  // rat.graphics.beginBitmapFill(loader.getResult("rat")).drawRect(0, 0, 60, 60);
-	
+
 	  const SpriteSheet = new createjs.SpriteSheet({
 	    framerate: 20,
 	    "images": [loader.getResult("rat")],
@@ -64,42 +64,42 @@
 	      "right": [12, 15]
 	    }
 	  });
-	
+
 	  rat = new createjs.Sprite(SpriteSheet, "up");
 	  rat.x = 250;
 	  rat.y = 500;
-	
+
 	  stage.addChild(rat);
-	
-	  stage.addEventListener("keypress", scurry);
-	
+
+	  stage.addEventListener("keydown", scurry);
+
 	  createjs.Ticker.timingMode = createjs.Ticker.RAF;
 	  createjs.Ticker.addEventListener("tick", tick);
-	
+
 	  // stage.update();
 	};
-	
+
 	function scurry() {
 	  console.log(event.target.value);
 	}
-	
+
 	function tick(event) {
 	  stage.update(event);
 	}
-	
+
 	function init() {
 	  const canvas = document.getElementById("ratterCanvas");
 	  stage = new createjs.Stage(canvas);
-	
+
 	  width = stage.canvas.width;
 	  height = stage.canvas.height;
-	
+
 	  const manifest = [{ src: "rat.png", id: "rat" }];
-	
+
 	  loader = new createjs.LoadQueue(false);
 	  loader.addEventListener("complete", handleComplete);
 	  loader.loadManifest(manifest, true, "./app/assets/images/");
-	
+
 	  // const ratData = {
 	  //   images: ["./app/assets/images/rat.png"],
 	  //   frames: {width:500, height:700},
@@ -110,8 +110,8 @@
 	  //   // }
 	  // };
 	  // const ratSprite = new createjs.Sprite(ratData, 0);
-	
-	
+
+
 	  // let circle = new createjs.Shape();
 	  // circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
 	  // circle.x = 100;
